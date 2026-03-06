732507d19a5cda527a1c92117581b7fc731a9ac3
dimitrisvagias <dimitris.vagias.02gmail.com>
2026-03-06 20:50:53 +0100

    gramm zu newton

--- views/umrechner.py
+++ views/umrechner.py
@@ -1,9 +1,24 @@
 import streamlit as st
 
-st.title("Gramm zu Newton Umrechner")
+st.title(" Gramm zu Newton Umrechner")
+st.divider()
+st.caption ("Gib eine Masse in Gramm ein oder benutze den Slider")
+gramm = st.slider("Slider:", min_value=0.0, max_value=10000.0, step=0.1)
 
-gramm = st.number_input("Gramm eingeben:", min_value=0.0, step=0.1)
+gramm = st.number_input("Oder Gramm eingeben:", min_value=0.0, max_value=10000.0, value=gramm, step=0.1)
 
 newton = gramm * 0.00980665
 
-st.write(f"{gramm} g = **{newton:.4f} N**")
+st.divider ()
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric(label="Gramm", value=f"{gramm:.1f} g")
+
+with col2:
+    st.metric(label="Newton", value=f"{newton:.4f} N")
+
+st.divider()
+
