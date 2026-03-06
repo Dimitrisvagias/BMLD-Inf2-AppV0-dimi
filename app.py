954f69fb6dd97894cc23961d75774e2370fa63a6
dimitrisvagias <dimitris.vagias.02gmail.com>
2026-03-07 00:20:38 +0100

    rechner kg zu newton

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:")
 pg_second = st.Page("views/addition_calculator.py", title="Plus_Rechner", icon=":material/calculate:")
 pg_third = st.Page("views/Knopf_Druck.py", title="Klick-Zähler", icon=":material/ads_click:")
-pg_fourth = st.Page("views/umrechner.py", title="Gramm zu Newton", icon=":material/scale:")
+pg_fourth = st.Page("views/umrechner.py", title="Kilogramm zu Newton", icon=":material/scale:")
 
 pg = st.navigation([pg_home, pg_second, pg_third, pg_fourth])
 pg.run()
--- views/umrechner.py
+++ views/umrechner.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.title(" Gramm zu Newton Umrechner")
+st.title(" Kilogramm zu Newton Umrechner")
 st.divider()
-st.caption ("Gib eine Masse in Gramm ein oder benutze den Slider")
-gramm = st.slider("Slider:", min_value=0.0, max_value=10000.0, step=0.1)
+st.caption ("Gib eine Masse in Kilogramm ein oder benutze den Slider")
+Kilogramm = st.slider("Slider:", min_value=0.0, max_value=100.0, step=0.1)
 
-gramm = st.number_input("Oder Gramm eingeben:", min_value=0.0, max_value=10000.0, value=gramm, step=0.1)
+Kilogramm = st.number_input(" Kilogramm eingeben:", min_value=0.0, max_value=100.0, value=Kilogramm, step=0.1)
 
-newton = gramm * 0.00980665
+newton = Kilogramm * 9.80665
 
 st.divider ()
 
@@ -15,10 +15,11 @@ st.divider ()
 col1, col2 = st.columns(2)
 
 with col1:
-    st.metric(label="Gramm", value=f"{gramm:.1f} g")
+    st.metric(label="Kilogramm", value=f"{Kilogramm:.1f} kg")
 
 with col2:
     st.metric(label="Newton", value=f"{newton:.4f} N")
 
 st.divider()
 
+st.info("⚠️ Diese Umrechnung gilt nur auf der Erde! Die Gravitationsbeschleunigung beträgt 9.80665 m/s² – auf dem Mond oder anderen Planeten wäre das Ergebnis anders.")
